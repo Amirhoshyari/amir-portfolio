b7c9d91377a361ceb041ec403052cc9fcfacf8bc
Amirhoshyari <amirhoshyari30@gmail.com>
2026-09-24 10:29:59 -0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = "amir-portfolio-v1";
+
+const FILES_TO_CACHE = [
+  "/amir-portfolio/",
+  "/amir-portfolio/index.html",
+  "/amir-portfolio/profile.png",
+  "/amir-portfolio/manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+});
